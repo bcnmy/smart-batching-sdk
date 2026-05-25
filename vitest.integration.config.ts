e84a2d7addfe7826100e15c2fe4e2f4ed14652b2
Venkatesh <rvenki666@gmail.com>
2026-05-25 17:24:35 +0530

    feat: fixed test path config

--- vitest.integration.config.ts
+++ vitest.integration.config.ts
@@ -1,7 +1,13 @@
+import { resolve } from 'node:path';
 import { loadEnv } from 'vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig(({ mode }) => ({
+  resolve: {
+    alias: {
+      'smart-batching': resolve(__dirname, 'src/index.ts'),
+    },
+  },
   test: {
     env: loadEnv(mode, process.cwd(), ''),
     testTimeout: 500_000,
